36b61e1293bfc4c6b204d54822be670b9a82ae24
siddhant221 <55878798+siddhant221@users.noreply.github.com>
2023-09-10 13:27:39 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+public class Factorial {
+     public static void main(String[] args) {
+          int n=5;
+          int fact=1;
+          for(int i =2;i<=n;i++)
+          {
+              fact =fact * i;
+          }
+          System.out.println(fact);
+          
+     }
+     
+}
+/*
+ * 5=4 x 3 x 2 x 1
+fact = 1 
+for(int i=2;i=n;i++)
+
+
+ */
